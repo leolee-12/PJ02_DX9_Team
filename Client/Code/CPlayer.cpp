0bcbd1af42f2521fd79fcbc4bd6eb815e89b7bf9
Leo <leo.lee.dec@gmail.com>
2025-12-16 22:47:59 +0900

    플레이어 수정 시작

--- Client/Code/CPlayer.cpp
+++ Client/Code/CPlayer.cpp
@@ -6,13 +6,23 @@
 #include "CDInputMgr.h"
 
 CPlayer::CPlayer(LPDIRECT3DDEVICE9 pGraphicDev, IMessageChannel* StageChannel)
-	: CGameObject(pGraphicDev, StageChannel)
+	:	CGameObject(pGraphicDev, StageChannel),
+		m_ePreState(PS_END),
+		m_eCurState(PS_IDLE),
+		m_fFrame(0.f),
+		m_fFrameSpeed(0.f)
 {
+	ZeroMemory(m_fFrameEnd, sizeof(m_fFrameEnd));
 }
 
-CPlayer::CPlayer(const CGameObject& rhs)
-	: CGameObject(rhs)
+CPlayer::CPlayer(const CPlayer& rhs)
+	:	CGameObject(rhs),
+		m_ePreState(PS_END),
+		m_eCurState(PS_IDLE),
+		m_fFrame(0.f),
+		m_fFrameSpeed(0.f)
 {
+	memcpy(m_fFrameEnd, rhs.m_fFrameEnd, sizeof(m_fFrameEnd));
 }
 
 CPlayer::~CPlayer()
@@ -41,6 +51,9 @@ HRESULT CPlayer::Ready_GameObject()
 
 _int CPlayer::Update_GameObject(const _float& fTimeDelta)
 {
+	Check_Frame();
+	Move_Frame(fTimeDelta);
+
 	_int iExit = CGameObject::Update_GameObject(fTimeDelta);
 	
 	Set_OnTerrain();
@@ -64,7 +77,8 @@ void CPlayer::Render_GameObject()
 
 	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
 		
-	m_pTextureCom->Set_Texture(0);
+	m_pTextureCom->Set_Texture(_uint(m_fFrame));
+	
 	m_pBufferCom->Render_Buffer();
 
 	m_pGraphicDev->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
@@ -203,6 +217,31 @@ _vec3 CPlayer::Picking_OnTerrain()
 		return _vec3();
 
 	return m_pCalculatorCom->Picking_OnTerrain(g_hWnd, pTerrainVtxCom, pTerrainTransformCom);
+}
+
+void CPlayer::Check_Frame()
+{
+	if (m_ePreState == m_eCurState)
+		return;
+
+	switch (m_eCurState)
+	{
+	case PS_IDLE:
+	{
+
+	}
+	break;
+	}
+
+	m_ePreState = m_eCurState;
+}
+
+void CPlayer::Move_Frame(const _float& fTimeDelta)
+{
+	m_fFrame += m_fFrameSpeed * fTimeDelta;
+
+	if (m_fFrame > m_fFrameEnd[m_eCurState])
+		m_fFrame = 0.f;
 }
 
 CPlayer* CPlayer::Create(LPDIRECT3DDEVICE9 pGraphicDev, IMessageChannel* StageChannel)
--- Client/Header/CPlayer.h
+++ Client/Header/CPlayer.h
@@ -11,9 +11,12 @@ namespace Engine
 
 class CPlayer : public CGameObject
 {
+public:
+	enum PLAYERSTATE { PS_IDLE, PS_RUN, PS_ROLL, PS_ATTACK, PS_WORK, PS_TALK, PS_INTROIDLE, PS_INTRORUN, PS_INTROKNEE, PS_END };
+
 private:
 	explicit CPlayer(LPDIRECT3DDEVICE9 pGraphicDev, IMessageChannel* StageChannel);
-	explicit CPlayer(const CGameObject& rhs);
+	explicit CPlayer(const CPlayer& rhs);
 	virtual ~CPlayer();
 
 public:
@@ -28,11 +31,20 @@ private:
 	void			Set_OnTerrain();
 	_vec3			Picking_OnTerrain();
 
+	void			Check_Frame();
+	void			Move_Frame(const _float& fTimeDelta);
+
 private:
 	Engine::CRcTex*			m_pBufferCom;
 	Engine::CTransform*		m_pTransformCom;
 	Engine::CTexture*		m_pTextureCom;
 	Engine::CCalculator*	m_pCalculatorCom;
+
+	PLAYERSTATE		m_ePreState;
+	PLAYERSTATE		m_eCurState;
+	_float			m_fFrame;
+	_float			m_fFrameSpeed;
+	_float			m_fFrameEnd[PS_END];
 
 public:
 	static CPlayer* Create(LPDIRECT3DDEVICE9 pGraphicDev, IMessageChannel* StageChannel);
