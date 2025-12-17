e608e48cfc1462b13a72b6404649dfd1b09b08db
Marb1e0817 <mabll0817@gmail.com>
2025-12-17 22:01:27 +0900

    imGui / Font 적용 (#12)

--- Client/Code/CImGuiManager.cpp
+++ Client/Code/CImGuiManager.cpp
@@ -48,6 +48,8 @@ void CImGuiManager::ImGui_Setup(HWND hWnd, LPDIRECT3DDEVICE9 pDevice)
     ImGui_ImplWin32_Init(g_hWnd);
     ImGui_ImplDX9_Init(m_pGraphicDev);
 
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("../Font/arial.ttf", 16.0f);
 
     char buf[128];
     sprintf_s(buf, "ImGui Manager Init HWND = %p\n", g_hWnd);
