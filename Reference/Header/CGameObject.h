ffdb467249ff521a2c32d4bf22d36d46a508c0ed
LEO <leo.lee.dec@gmail.com>
2025-12-26 02:04:02 +0900

    [docs] 개인 작업 현황 기록용 md 파일 추가

--- Reference/Header/CGameObject.h
+++ Reference/Header/CGameObject.h
@@ -14,14 +14,9 @@ protected:
 	virtual ~CGameObject();
 
 public:
-<<<<<<< HEAD
-	void		Set_MessageChannel(IMessageChannel* pChannel) 
-	{
-=======
 	void        Set_MessageChannel(IMessageChannel* pChannel)
 	{
 		if (pChannel == nullptr) { return; }
->>>>>>> main
 		if (m_pMessageChannel) {
 			Safe_Release(m_pMessageChannel);
 		}
