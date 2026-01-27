8aabc13095ae4db4f8813cd1662abf7c49d7269a
Marb1e0817 <mabll0817@gmail.com>
2026-01-28 03:54:14 +0900

    [feat] 던전 추종자 영입 상호작용 추가 및 빌딩상태 카메라 움직임 구현

--- Client/Code/CVillage.cpp
+++ Client/Code/CVillage.cpp
@@ -645,6 +645,8 @@ void CVillage::Ready_Event_Village()
 
 void CVillage::Key_Input_Village()
 {
+	IMessageChannel::EVENT SceneEvent;
+
 	// 디버그 키인풋 윤석현
 	if (CDInputMgr::GetInstance()->Key_Down(DIK_F9))
 	{
@@ -652,6 +654,8 @@ void CVillage::Key_Input_Village()
 		{
 			m_pCurBuilding = CBuilding::Create(m_pGraphicDev, m_pMessageChannel, _vec3(175.5f, -0.95f, 40.f), CBuilding::BT_KNUCKLEBONE, CBuilding::BS_PREVIEW);
 
+			SceneEvent.strType = L"Building.Enter";
+			m_pMessageChannel->Publish(SceneEvent);
 			m_bBuildingFlag = true;
 		}
 	}
@@ -661,6 +665,8 @@ void CVillage::Key_Input_Village()
 		{
 			Safe_Destroy(m_pCurBuilding);
 			m_bBuildingFlag = false;
+			SceneEvent.strType = L"Building.Exit";
+			m_pMessageChannel->Publish(SceneEvent);
 		}
 		else if (CDInputMgr::GetInstance()->Mouse_Down(DIM_LB))
 		{
@@ -675,6 +681,9 @@ void CVillage::Key_Input_Village()
 
 				m_pCurBuilding = nullptr;
 				m_bBuildingFlag = false;
+
+				SceneEvent.strType = L"Building.Exit";
+				m_pMessageChannel->Publish(SceneEvent);
 			}
 		}
 	}
